bc1d4bfc4a72ed2642a7e7ffbbbc34bdc8f8ff40
Wray Bowling <wraywraybowling.com>
2018-12-24 12:01:15 -0500

    ADCPi integrated with glslViewer python wrapper

--- software/pythonWrapper/demo.py
+++ software/pythonWrapper/demo.py
@@ -1,7 +1,24 @@
-import glslviewer
+#!/usr/bin/env python
+"""
+MicroWrave Video
 
-print ( dir(glslviewer.absolute_import) )
+Required installed binaries:
+    - glslViewer
+    - glslViewer Python library
+One of the required hardware:
+    - ABElectronics ADCPi or
+    - the MicroWrave Video module
+"""
 
-root = '/home/pi/MicroWrave/'
-glslviewer.absolute_import(root + 'assets/fragment-shaders/tarot/01-justice.frag')
+from glslviewer import GlslViewer
+import time
+from ADCPi import ADCPi
+
+gv = GlslViewer('../../assets/fragment-shaders/Villarosa/scope.frag', {})
+gv.start()
+adc = ADCPi(0x68, 0x69, 12)
+
+while True:
+    gv.setUniform('u_analog1', adc.read_voltage(1) / 5.0);
+    gv.setUniform('u_analog2', adc.read_voltage(2) / 5.0);
 
